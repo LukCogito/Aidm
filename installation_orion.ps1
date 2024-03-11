dd26018d04aeb21270fa818977f4c3ce95dded6a
Luk Cogito <luk.cogito@pilsfree.net>
2024-03-11 14:25:51 +0100

    Installator for orion made fully automated.

--- installation_orion.ps1
+++ installation_orion.ps1
@@ -1,21 +1,46 @@
-# Installation script for dependencies of Aim
+# Installation script for dependencies of Aidm
 
 echo "Installing Python..."
 
-# Install python from samba
-# Source: https://www.python.org/downloads/release/python-3118/
-\path\to\python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+# Set an URL for downloading Python
+$python_url = "https://www.python.org/ftp/python/3.11.8/python-3.11.8-amd64.exe"
+
+# Set a path for downloading Python
+$installer = "$env:TEMP\python-3.11.8-amd64.exe"
+
+# Download Python installer
+Invoke-WebRequest -Uri $python_url -OutFile $installer
+
+# Install Python silently
+$installer /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+# Remove the installer
+Remove-Item $installer
+
 
 echo "Installing pip..."
 
 # Check if the version of pip is up to date and install lastest if not
 py -m pip install --upgrade pip
 
+
 echo "Installing cmake..."
 
-# Install cmake
-# Source: https://cmake.org/download/
-msiexec /i \path\to\cmake.msi /qn /norestart
+# Set an URL for downloading cmake
+$cmake_url = "https://github.com/Kitware/CMake/releases/download/v3.29.0-rc3/cmake-3.29.0-rc3-windows-x86_64.msi"
+
+# Set a path for downloading cmake
+$installer = "$env:TEMP\cmake-3.29.0-rc3-windows-x86_64.msi"
+
+# Download cmake installer
+Invoke-WebRequest -Uri $cmake_url -OutFile $installer
+
+# Install cmake silently
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $installer /qn /norestart" -Wait
+
+# Remove the installer
+Remove-Item $installer
+
 
 echo "Defining paths..."
 
@@ -23,7 +48,24 @@ echo "Defining paths..."
 $Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
 
 # Define the path to the 7-Zip directory
-$7ZipPath = "C:\Program Files\7-Zip\"
+$7Zip_path = "C:\Program Files\7-Zip\"
+
+# Verify if path to 7zip exists
+if(-not (Test-Path -Path $7Zip_path)){
+    # Define the download URL and installer location
+    $7zip_url = "https://www.7-zip.org/a/7z2401-x64.msi"
+    $installer = "$env:TEMP\7z2401-x64.msi"
+    
+    # Download 7-Zip installer
+    Invoke-WebRequest -Uri $7zip_url -OutFile $installer
+    
+    # Install 7-Zip silently
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $installer /qn" -Wait
+    
+    # Remove the installer
+    Remove-Item $installer
+}
+
 
 # Define the path to the cmake directory
 $cmakePath = "C:\Program Files\CMake\bin\"
@@ -50,25 +92,54 @@ $UserPath = [System.Environment]::GetEnvironmentVariable("Path", "User")
 # Combine the Machine and User paths, and update the current session's Path environment variable
 $Env:Path = $MachinePath + ";" + $UserPath
 
+
 echo "Installing ffmpeg..."
 
 # Install ffmpeg (free and open-source software project consisting of a suite of libraries and programs for handling video, audio, and other multimedia files and streams)
-Invoke-WebRequest -Uri "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z" -OutFile "ffmpeg.7z"
-7zip x ffmpeg.7z
-$env:Path += ";$($pwd)\ffmpeg\bin"
+# Set an URL for downloading ffmpeg
+$ffmpeg_url = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z"
+
+# Set a path for downloading ffmpeg
+$zip_file = "$env:TEMP\ffmpeg-release-full.7z"
+
+# Set a path for extracting ffmpeg
+$ffmpeg_extraction_path = "C:\Program Files\ffmpeg-6.1.1-full_build"
+
+# Download ffmpeg zip file
+Invoke-WebRequest -Uri $ffmpeg_url -OutFile $zip_file
+
+# Extract ffmpeg silently
+7zip x $zip_file $ffmpeg_extraction_path
+
+# Remove the zip file
+Remove-Item $zip_file
+
 
 echo "Installing VS Build Tools for C++ ..."
 
 # Install Visual Studio Build Tools for C++
 # Source: https://visualstudio.microsoft.com/cs/visual-cpp-build-tools/
-Start-Process vs_BuildTools.exe -ArgumentList '--quiet', '--wait', '--norestart', '--nocache', '--installPath C:\BuildTools', '--add Microsoft.VisualStudio.Workload.VCTools', '--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64', '--add Microsoft.VisualStudio.Component.VC.ATL', '--add Microsoft.VisualStudio.Component.VC.CLI.Support', '--add Microsoft.VisualStudio.Component.Windows10SDK.18362' -NoNewWindow -Wait
+
+# Set an URL for downloading vs
+$vs_url = "https://aka.ms/vs/17/release/vs_BuildTools.exe"
+
+# Set a path for downloading vs
+$installer = "$env:TEMP\vs_BuildTools.exe"
+
+# Download vs installer
+Invoke-WebRequest -Uri $vs_url -OutFile $installer
+
+# Install vs silently
+Start-Process $installer -ArgumentList '--quiet', '--wait', '--norestart', '--nocache', '--installPath C:\BuildTools', '--add Microsoft.VisualStudio.Workload.VCTools', '--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64', '--add Microsoft.VisualStudio.Component.VC.ATL', '--add Microsoft.VisualStudio.Component.VC.CLI.Support', '--add Microsoft.VisualStudio.Component.Windows10SDK.18362' -NoNewWindow -Wait
+
+# Remove the installer
+Remove-Item $installer
+
 
 echo "Installing essential Python packages via pip..."
 
 # Install PyTorch
 pip install torch torchvision torchaudio
-
-# Installing collected packages: mpmath, urllib3, typing-extensions, sympy, pillow, numpy, networkx, MarkupSafe, idna, fsspec, filelock, charset-normalizer, certifi, requests, jinja2, torch, torchvision, torchaudio
 
 # Install backgroundremover
 pip install backgroundremover
