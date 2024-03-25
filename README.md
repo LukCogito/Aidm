91eda21928985d1af3c56c154867579eb90c0ecf
Luk Cogito <luk.cogito@pilsfree.net>
2024-03-25 15:33:36 +0100

    TODO updated and image_conversion.py modified.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - ~~replace ffmpeg usage with python-ffmpeg usage~~
 - make relative paths functional for all users or change to absolute paths
 - make all all scripts execuatble as non-administrator
-	+ Possible way may be by changing `$U2NET_PATH`
+	+ Possible way may be by changing `$U2NET_PATH` and re-locating files to `D:\`
 - Fix this error
 ```
 param : The term 'param' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a path was included, verify that the path is correct and try again.
--- standard_procedure/image_conversion.py
+++ standard_procedure/image_conversion.py
@@ -1,4 +1,4 @@
-import ffmpeg
+from ffmpeg import FFmpeg
 import sys
 
 # Get an input path from a command line argument
@@ -7,9 +7,17 @@ input_path = sys.argv[1]
 # Get an output path from a command line argument
 output_path = sys.argv[2]
 
-# Do the conversion
-(
-	ffmpeg.input(input_path)
-	.output(output_path)
-	.run()
-)
+# Define the main funciton for the conversion
+def main():
+	ffmpeg = (
+		FFmpeg()
+		.option("i")
+		.input(input_path)
+		.output(output_path)
+	)
+
+	ffmpeg.execute()
+
+# If script is executed as main (and not as an module)
+if __name__ == "__main__":
+	main()
