5ab9bac6713c01b2614ad204b440d33a3cb6eab0
Luk Cogito <luk.cogito@pilsfree.net>
2024-03-26 12:56:02 +0100

    File conversion updated.

--- standard_procedure/image_conversion.py
+++ standard_procedure/image_conversion.py
@@ -1,4 +1,4 @@
-from ffmpeg import FFmpeg
+import ffmpeg
 import sys
 
 # Get an input path from a command line argument
@@ -7,17 +7,10 @@ input_path = sys.argv[1]
 # Get an output path from a command line argument
 output_path = sys.argv[2]
 
-# Define the main funciton for the conversion
-def main():
-	ffmpeg = (
-		FFmpeg()
-		.option("i")
-		.input(input_path)
-		.output(output_path)
-	)
 
-	ffmpeg.execute()
-
-# If script is executed as main (and not as an module)
-if __name__ == "__main__":
-	main()
+(
+    ffmpeg
+    .input(input_path)
+    .output(output_path)
+    .run()
+)
